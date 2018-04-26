d7eb3c46ad0bd0eb359141b226b4655dd9905f5c
Jakub Safar <jakubsafar@jsa-activate.local>
2018-04-26 19:14:16 +0200

    Just changes in comment.

--- hello-world.js
+++ hello-world.js
@@ -7,11 +7,12 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   // Send the response body as "An evil Hello World"
+   response.end('Muhaha World\n');
 }).listen(8081);
 
 // Console will print the message
+// ... any port would do
 console.log('Server running at http://127.0.0.1:8081/');
 
 // Testing the git changes
